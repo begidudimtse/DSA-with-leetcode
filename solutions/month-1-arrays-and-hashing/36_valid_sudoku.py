23549d501161557b2a6e03f3c7477d6b47adfc36
Dimtse Begidu <begidudimtse@gmail.com>
2026-08-19 12:16:47 +0300

    valid sudoku solution

---
+++ solutions/month-1-arrays-and-hashing/36_valid_sudoku.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+
+        # 1. Check Rows
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+            
+        # 2. Check Columns
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+                
+        # 3. Check 3x3 Sub-boxes
+        starts = [
+            (0, 0), (0, 3), (0, 6),
+            (3, 0), (3, 3), (3, 6),
+            (6, 0), (6, 3), (6, 6)
+        ]
+
+        for i, j in starts:
+            s = set()
+            for row in range(i, i + 3):
+                for col in range(j, j + 3):
+                    item = board[row][col]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+                        
+        return True
